f78a750fbd869c619408597056ef968773c08a76
Maxwell Austensen <maxwell.austensen@nyu.edu>
2019-09-25 17:14:40 -0400

    split up sql view and triggers

--- sql/defacto_bk_bbl_details.sql
+++ sql/defacto_bk_bbl_details.sql
@@ -136,30 +136,3 @@ CREATE MATERIALIZED VIEW defacto_bk_bbl_details AS (
 		)
 	ORDER BY p.bbl
 );
-
-
-CREATE OR REPLACE FUNCTION refresh_defacto_bk_bbl_details() 
-RETURNS TRIGGER LANGUAGE plpgsql 
-AS $$ 
-BEGIN 
-	REFRESH MATERIALIZED VIEW defacto_bk_bbl_details; 
-	RETURN NULL; 
-END $$;
-
-
-CREATE TRIGGER refresh_defacto_bk_bbl_details 
-AFTER INSERT OR UPDATE OR DELETE OR TRUNCATE 
-ON ecb_violations FOR EACH STATEMENT 
-EXECUTE PROCEDURE refresh_defacto_bk_bbl_details();
-
-
-CREATE TRIGGER refresh_defacto_bk_bbl_details 
-AFTER INSERT OR UPDATE OR DELETE OR TRUNCATE 
-ON oath_hearings FOR EACH STATEMENT 
-EXECUTE PROCEDURE refresh_defacto_bk_bbl_details();
-
-
-CREATE TRIGGER refresh_defacto_bk_bbl_details 
-AFTER INSERT OR UPDATE OR DELETE OR TRUNCATE 
-ON hpd_complaints 
-FOR EACH STATEMENT EXECUTE PROCEDURE refresh_defacto_bk_bbl_details();
